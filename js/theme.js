cf877ebff741303be4039044ed2653eb71264cc5
knnai <81048686+knnai@users.noreply.github.com>
2021-12-04 14:55:37 +0200

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -5,51 +5,56 @@ document.addEventListener("DOMContentLoaded", function (e) {
 	//обработчик события 
 	let themeInput = document.querySelector("#themeInput");
 	if (themeInput) themeInput.addEventListener('change', changeTheme);
-	
+
 	//<link id="linkTheme" rel="stylesheet" href="css/variables_{  ...  }.css">
-	let linkTheme =  document.querySelector("#linkTheme");
-	
+	let linkTheme = document.querySelector("#linkTheme");
+
 	//инициализация
-	let myStorage = window.localStorage;		
-	let colorTheme = myStorage.getItem('theme');	
-	if (!colorTheme){
+	let myStorage = window.localStorage;
+	let colorTheme = myStorage.getItem('theme');
+	if (!colorTheme) {
 		colorTheme = 'dark';
-		myStorage.setItem('theme', colorTheme);		
-	};	
-	linkTheme.href = 'css/variables_'+colorTheme+'.css'
-	themeInput.checked = (colorTheme == 'light') ? true : false;	
+		myStorage.setItem('theme', colorTheme);
+	};
+	linkTheme.href = 'css/variables_' + colorTheme + '.css'
+	themeInput.checked = (colorTheme == 'light') ? true : false;
 	changePage(colorTheme);
 	//console.dir('init > '+colorTheme+' > '+themeInput.checked);
-	
+
 	//изменение темы
-	function changeTheme(e){
+	function changeTheme(e) {
 		colorTheme = (e.target.checked) ? 'light' : 'dark';
-		myStorage.setItem('theme', colorTheme);			
-		linkTheme.href = 'css/variables_'+colorTheme+'.css'		
+		myStorage.setItem('theme', colorTheme);
+		linkTheme.href = 'css/variables_' + colorTheme + '.css'
 		changePage(colorTheme);
 		//console.dir('change > '+e.target.checked+' > '+colorTheme);
 	};
-	
-	function changePage(colorTheme){
+
+	function changePage(colorTheme) {
 		//метки выбора темы
 		let labelsTheme = document.querySelectorAll(".theme__label");
-		if (labelsTheme){
-			for (let elem of labelsTheme){
-				console.dir(elem.style.backgroundImage);
+		if (labelsTheme) {
+			for (let elem of labelsTheme) {
+				//console.dir(elem.style.backgroundImage);
 				elem.style.backgroundImage = (colorTheme == 'light') ? "url('img/dark-mode.svg')" : "url('img/light-mode.svg')";
 				elem.title = (colorTheme == 'light') ? "Темная тема" : "Светлая тема";
-				console.dir(elem.style.backgroundImage);
-			};			
-		}		
+				//console.dir(elem.style.backgroundImage);
+			};
+		}
 		//декор на слайдах главной страницы
 		let slideDecors = document.querySelectorAll('.slide__decor');
-		if (slideDecors){
-			for (let elem of slideDecors){
+		if (slideDecors) {
+			for (let elem of slideDecors) {
 				elem.src = (colorTheme == 'light') ? "img/light-decor.png" : "img/dark-decor.png";
 			};
 		};
+		//превью страницы сервисов
+		let previewImg = document.querySelector('#frmService .preview img');
+		if (previewImg) {
+			previewImg.src = (colorTheme == 'light') ? "img/gallery/design-default-light.jpg" : "img/gallery/design-default.jpg";
+		}
 	};
-	
-	
-	/*-------------------цветовая тема---------------------------*/	
-});	
+
+
+	/*-------------------цветовая тема---------------------------*/
+});
